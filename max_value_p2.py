6d2b534abf35a0eae46eefafb758e7beef9b9002
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-12 12:25:02 -0700

    done max_value_p2

--- max_value_p2.py
+++ max_value_p2.py
@@ -1,11 +1,19 @@
-''' 
+'''
 Write a function, max_value, that takes in list of numbers as an argument. The function should return the largest number in the list.
+
+Solve this without using any built-in list methods.
 
 You can assume that the list is non-empty.
 '''
 
 def max_value(nums):
-    return max(nums)
+    max = -1 
+    for num in nums:
+        if num > max:
+            max = num
+        elif num == max:
+            max = num
+    return max
 
 
 print(max_value([4, 7, 2, 8, 10, 9]))
