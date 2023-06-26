e3e75fa27f9bcf86d91f67dc5d8d5803381a0c0f
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-26 14:27:06 -0700

    done

--- 7.pair_sum_p2.py
+++ 7.pair_sum_p2.py
@@ -5,18 +5,18 @@ Be sure to return the indices, not the elements themselves.
 
 There is guaranteed to be one such pair that sums to the target.
 
-'''
-def pair_sum(lst, target):
-    for i in range(len(lst)):
-        print('i', i)
-        for j in range(i + 1, len(lst)):
-            if lst[i] + lst[j] == target:
-                return (i, j)
+using hash map is a O(1) lookup
 
+'''
+
+def pair_sum(lst, target):
+    pair_sum_dict ={}
+    for index, number in enumerate(lst):
+        complement = target - number 
+
+        if complement in pair_sum_dict:
+            return(pair_sum_dict[complement], index)
+        pair_sum_dict[number] = index
+        
 
 print(pair_sum([3,2,5,4,1], 8))
-print(pair_sum([4, 7, 9, 2, 5, 1], 5)) # -> (0, 5)
-print(pair_sum([4, 7, 9, 2, 5, 1], 3))
-print(pair_sum([1, 6, 7, 2], 13))
-print(pair_sum([9, 9], 18))
-print(pair_sum([6, 4, 2, 8 ], 12))
