d19b8648fe927de15889503889a60baa91600b8a
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-06-27 11:42:23 -0700

    jewels and stones

---
+++ Jewels_stones.py
@@ -0,0 +1,8 @@
+def numJewelsInStones(jewels, stones):
+    count = 0
+    for char in stones:
+        if char in jewels:
+            count+=1
+    return count
+
+print(numJewelsInStones("z", "ZZ" ))
